664331b9ff3a7cf91556dc2c4a179e97260fa70d
Allie LeFever <alefever39@gmail.com>
2022-05-22 14:22:28 -0600

    commit tree 75aa61759bb32119aaf1fd1582c6096cd7583f0e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, email) {
+    const userData = {
+        'name': userName,
+        'email': email
+    };
+    const body = document.querySelector('body');
+    
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        const h2 = document.createElement('h2');
+        h2.textContent = data.id;
+        body.append(h2);
+    })
+    .catch(error => {
+        const h2 = document.createElement('h2');
+        h2.textContent = error.message;
+        body.append(h2);
+    })
+}
